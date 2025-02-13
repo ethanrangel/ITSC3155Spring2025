eb6d8fe8c77fddc21147afd4bd94d9b806d4616c
IrfanCamparaGithub <irfancampara1@gmail.com>
2025-02-12 19:10:27 -0500

    feat: add feature to print customer information for checkingAccount

--- BankAccountPart2/CheckingAccount.py
+++ BankAccountPart2/CheckingAccount.py
@@ -3,4 +3,12 @@ class CheckingAccount(BankAccount):
    def __init__(self, customer_name, current_balance, minimum_balance, transfer_limit):
        super().__init__(customer_name, current_balance, minimum_balance)
        self.transfer_limit = transfer_limit
-
+def print_customer_information(self):
+       print(
+           '''
+           Hello {}, Thank you for using {}. Here is your checking account information
+           Current Balance: ${}
+           Minimum Balance: ${}
+           Transfer Limit: ${} %
+           '''.format(self.customer_name, self.bank_title, self.current_balance, self.minimum_balance, self.transfer_limit)
+       )
