607ec100fc2793d98686abdc99b1ae4ad9f2bafe
IrfanCamparaGithub <irfancampara1@gmail.com>
2025-02-19 12:49:33 -0500

    feat add addition and division function

--- HorribleCodeActivity/FixedCode.py
+++ HorribleCodeActivity/FixedCode.py
@@ -1,2 +1,19 @@
-# This will be fixed code
-x = "Hello World"
+def main():
+
+    pass
+
+
+
+
+def divide(number1, number2):
+    """Return a int
+    Division result between two numbers (number1 and number2)
+    """
+    return number1 / number2
+
+
+def add(number1, number2): 
+    """Return a int
+    Addition result between two numbers (number1 and number2)
+    """
+    number1 + number2
