29a0f743d0b72b4203fea7adb4b0829ce33da415
IrfanCamparaGithub <irfancampara1@gmail.com>
2025-02-19 12:50:13 -0500

    feat add subtraction and multiplication function

--- HorribleCodeActivity/FixedCode.py
+++ HorribleCodeActivity/FixedCode.py
@@ -17,3 +17,15 @@ def add(number1, number2):
     Addition result between two numbers (number1 and number2)
     """
     number1 + number2
+    
+def subtract(number1, number2):
+    """Return a int
+    Subtraction result between two numbers (number1 and number2)
+    """
+    return number1 - number2
+
+def multiply(number1, number2):
+    """Return a int
+    Multiplication result between two numbers (number1 and number2)
+    """
+    return number1 * number2
