289757f427e9fad6c81519c3a1dd87adc0b3cc5c
Ethan Rangel <ethanrangel13@gmail.com>
2025-02-19 13:14:00 -0500

    Fet: Fixing input type errors and accounting for different inputs

--- HorribleCodeActivity/FixedCode.py
+++ HorribleCodeActivity/FixedCode.py
@@ -1,43 +1,45 @@
+# This will be fixed code
 def main():
-
-    pass
-
-def main():
-
+    
     answer = ""
-
+    
     while answer != "off":
-
-        desiredInput = input("What operation do you want to do with this calculator add, subtract, multiply, divide or off: ")
-
+        
+        desiredInput = input("What operation do you want to do with this calculator add, subtract, multiply, divide or off: ").lower()
+        
         if desiredInput == "off":
-
+            
             print("You are already tired of making calculations...... well have a good day!!!")
             break
-
+        
         else:
-
-            number1 = input("what is the first number you want to perform the operation on")
-            number2 = input("what is the second number you want to perform the operation on")
-
-
+            
+            number1 = float(input("what is the first number you want to perform the operation on"))
+            number2 = float(input("what is the second number you want to perform the operation on"))
+            
+            
         if desiredInput == "add":
-
+            
             print("The result is: " + str(add(number1, number2)))
-
+            
         if desiredInput == "subtract":
-
+            
             print("The result is: " + str(subtract(number1, number2)))
-
+            
         if desiredInput == "multiply":
-
+            
             print("The result is: " + str(multiply(number1, number2)))
-
+            
         if desiredInput == "divide":
-
+            
             print("The result is: " + str(divide(number1, number2)))
-
-
+        
+        
+        
+        
+        
+        
+        
 def divide(number1, number2):
     """Return a int
     Division result between two numbers (number1 and number2)
@@ -49,8 +51,8 @@ def add(number1, number2):
     """Return a int
     Addition result between two numbers (number1 and number2)
     """
-    number1 + number2
-    
+    return number1 + number2
+        
 def subtract(number1, number2):
     """Return a int
     Subtraction result between two numbers (number1 and number2)
@@ -62,3 +64,6 @@ def multiply(number1, number2):
     Multiplication result between two numbers (number1 and number2)
     """
     return number1 * number2
+
+if __name__=="__main__":
+    main()
