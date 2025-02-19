7e97f46538216e2a8e64ef2981a145720a3a9a45
Ethan Rangel <ethanrangel13@gmail.com>
2025-02-19 11:27:00 -0500

    Add: Initial calculator structure (violates documentation and clean code)

--- HorribleCodeActivity/BadCode.py
+++ HorribleCodeActivity/BadCode.py
@@ -1,2 +1,23 @@
 # This will be our bad code
-x = "Hello World"
+##entire file no comments so violates documentation
+def main():
+    
+    answer = ""
+    
+    while answer != "off":
+        
+        desiredInput = input("What operation do you want to do with this calculator add, subtract, multiply, divide or off: ")
+        
+        if desiredInput == "off":
+            break
+        else:
+            number1 = input("what is the first number you want to perform the operation on")
+            number2 = input("what is the second number you want to perform the operation on")
+            
+            
+        possibleInputs15 = [add, subtract, multiply, divide]
+        possibleInputs155555= ['add', 'subtract', 'multiply', 'divide']
+        
+        print("Result" + str(possibleInputs15[possibleInputs155555.index(desiredInput)](int(number1), int(number2)))) #just horrible and not clean code in the slightest
+        
+        
